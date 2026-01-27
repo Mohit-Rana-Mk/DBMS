2ec93b7f1ce492431ee534e65a3f26c7e65b3258
Mohit Kumar Rana <mkrana.280504@gmail.com>
2026-01-27 22:32:43 +0530

    Update 06 How to Update Data in a MySQL Table.sql

--- Introduction to Databases - Self paced/06 How to Update Data in a MySQL Table.sql
+++ Introduction to Databases - Self paced/06 How to Update Data in a MySQL Table.sql
@@ -1,1 +1,28 @@
--- How to Update Data in a MySQL Table
+-- How to Update Data in a MySQL Table-- How to Update Data in a MySQL Table	
+
+-- Update a Single Row
+UPDATE students
+SET age = 20
+WHERE id = 7;
+
+-- Update Multiple Columns
+UPDATE students
+SET age = 18, name = 'Rinklepreet Kaur'
+WHERE id = 6;
+
+-- Update All Rows
+UPDATE students
+SET age = 20;
+
+-- Conditional Update with Comparison Operators
+UPDATE students
+SET grade = 'AI-DS'
+WHERE grade = 'B.Tech';
+
+-- Update Using IS NULL
+UPDATE students
+SET grade = 'Unknown'
+WHERE grade IS NULL;
+
+
+SELECT * FROM students;
